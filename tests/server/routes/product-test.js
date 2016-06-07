e0fc8905189a8e2b11390a1d502d8d21c9c09639
Jenna Zenk <zenkjenna@gmail.com>
2016-06-07 11:29:11 -0400

    route testing started

---
+++ tests/server/routes/product-test.js
@@ -0,0 +1,67 @@
+// Instantiate all models
+var expect = require('chai').expect;
+
+var Sequelize = require('sequelize');
+var dbURI = 'postgres://localhost:5432/testing-fsg';
+var db = new Sequelize(dbURI, {
+    logging: false
+});
+require('../../../server/db/models/product')(db);
+
+var supertest = require('supertest');
+
+describe('Products Route', function () {
+
+    var app, Product, product1, product2, guestAgent;
+
+    beforeEach('Sync DB', function () {
+        return db.sync({ force: true });
+    });
+
+    beforeEach('Create app', function () {
+        app = require('../../../server/app')(db);
+        Product = db.model('product');
+    });
+
+     beforeEach('Create a product', function () {
+        return Product.create({
+        	name: 'cigarets',
+        	description : 'marlboro', 
+        	price : 40, 
+        	inventory: 50
+        })
+        .then(function(p) {
+        	product1 = p;
+        	return Product.create({
+        		name : 'shagel',
+        		description: 'shagel vanilla',
+        		price : 5,
+        		inventory : 120
+        	})
+        })
+        .then(function(p){
+        	product2 = p;
+        })
+    });
+
+
+	beforeEach('Create guest agent', function () {
+		guestAgent = supertest.agent(app);
+	});
+
+    afterEach('Sync DB', function() {
+    	return db.sync({force : true});
+    })
+
+    it('GET all products', function(done) {
+    	agent.get('/api/products')
+    	.expect(200)
+    	.end(function(err,res){
+    		if(err) return done(err);
+    		expect(res.body).to.be.instanceof(Array);
+    		expect(res.body).to.have.length(2);
+    	})
+    })
+
+});
+
