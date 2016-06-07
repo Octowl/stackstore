31830fcf1a41a066aaad8164ae5f447e87736142
Octowl <alsaff1987@gmail.com>
2016-06-07 10:54:44 -0400

    Write product grid state and html

--- browser/js/productlist/productlist.js
+++ browser/js/productlist/productlist.js
@@ -1,21 +1,9 @@
 app.config(function ($stateProvider) {
-    $stateProvider.state('productlist', {
-        url: '/',
-        templateUrl: 'js/productlist/productlist.html'
-    })
-
-    .state('productlist.grid', {
+    $stateProvider.state('grid', {
     	url : '/grid',
     	templateUrl : 'js/productlist/productlistGrid.html',
     	controller : function($scope) {
-    		$scope.item = ['cigarets', 'showergel', 'perfume', 'panadol']
+    		$scope.products = ['cigarets', 'showergel', 'perfume', 'panadol']
     	}
     })
 });
-
-
-
-
-
-
-
