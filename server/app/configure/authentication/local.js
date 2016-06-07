24f34afff4dd3626c3bb693bb7de014504f0e136
COREY <coreygreenwald@Coreys-MacBook-Pro.local>
2016-06-07 11:27:47 -0400

    signup and login functional

--- server/app/configure/authentication/local.js
+++ server/app/configure/authentication/local.js
@@ -28,6 +28,37 @@ module.exports = function (app, db) {
 
     passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'}, strategyFn));
 
+    app.post('/signup', function (req, res, next) {
+        console.log('were at signup'); 
+        User.create(req.body)
+        .then(function(){
+            var authCb = function (err, user) {
+
+                if (err) return next(err);
+
+                if (!user) {
+                    var error = new Error('Invalid login credentials.');
+                    error.status = 401;
+                    return next(error);
+                }
+
+                // req.logIn will establish our session.
+                req.logIn(user, function (loginErr) {
+                    if (loginErr) return next(loginErr);
+                    // We respond with a response object that has user with _id and email.
+                    res.status(200).send({
+                        user: user.sanitize()
+                    });
+                });
+
+            };
+
+            passport.authenticate('local', authCb)(req, res, next);
+            
+        })
+       //.catch(next); 
+
+    });
     // A POST /login route is created to handle login.
     app.post('/login', function (req, res, next) {
 
