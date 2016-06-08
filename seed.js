7ea1ecb7342686dfc645b2c814663a4a0145be1b
Octowl <alsaff1987@gmail.com>
2016-06-08 19:02:46 -0400

    Update seed file for locations

--- seed.js
+++ seed.js
@@ -21,56 +21,72 @@ var chalk = require('chalk');
 var db = require('./server/db');
 var User = db.model('user');
 var Product = db.model('product');
+var Location = db.model('location');
 var Promise = require('sequelize').Promise;
 
-var seedProducts = function() {
+var seedProducts = function () {
 
-    var products = [
-        {
-            name : 'Marlboro Light',
-            description : 'cigarets',
-            price : 40,
-            inventory : 10
-        },
-        {
-            name : 'Shagel',
-            description : 'Shower Gel',
-            price : 5,
-            inventory : 100
-        },
-        {
-            name : 'Perfume 1',
-            description : 'Musc',
-            price : 40,
-            inventory : 10
-        }
-    ];
+    var products = [{
+        name: 'Marlboro Light',
+        description: 'cigarets',
+        price: 40,
+        inventory: 10
+    }, {
+        name: 'Shagel',
+        description: 'Shower Gel',
+        price: 5,
+        inventory: 100
+    }, {
+        name: 'Perfume 1',
+        description: 'Musc',
+        price: 40,
+        inventory: 10
+    }];
 
-    var creatingProducts = products.map(function(productObj){
+    var creatingProducts = products.map(function (productObj) {
         return Product.create(productObj);
-    })
+    });
 
     return Promise.all(creatingProducts);
-}
+};
+
+var seedLocations = function () {
+    var locations = [{
+        name: "Paris",
+        latitude: 48.8566,
+        longitude: 2.3522
+    }, {
+        name: "London",
+        latitude: 51.5074,
+        longitude: 0.1278
+    }, {
+        name: "New York",
+        latitude: 40.7128,
+        longitude: 74.0059
+    }];
+
+    var creatingLocations = locations.map(function (locationObj) {
+        return Location.create(locationObj);
+    });
+
+    return Promise.all(creatingLocations);
+};
 
 var seedUsers = function () {
 
-    var users = [
-        {
-            firstName: 'Foshizzle',
-            lastName: 'MaNizzle',
-            email: 'shizzle@fsa.com',
-            password: 'foshiz',
-            address: '17 Park Pl. Westbury, NY, 11213'
-        },
-        {
-            firstName: 'Barry',
-            lastName: 'O',
-            email: 'obama@gmail.com',
-            password: 'potus',
-            address: 'White House'
-        }
-    ];
+    var users = [{
+        firstName: 'Foshizzle',
+        lastName: 'MaNizzle',
+        email: 'shizzle@fsa.com',
+        password: 'foshiz',
+        address: '17 Park Pl. Westbury, NY, 11213'
+    }, {
+        firstName: 'Barry',
+        lastName: 'O',
+        email: 'obama@gmail.com',
+        password: 'potus',
+        address: 'White House'
+    }];
 
     var creatingUsers = users.map(function (userObj) {
         return User.create(userObj);
@@ -80,12 +96,17 @@ var seedUsers = function () {
 
 };
 
-db.sync({ force: true })
+db.sync({
+        force: true
+    })
     .then(function () {
         return seedUsers();
     })
-    .then(function(){
+    .then(function () {
         return seedProducts();
+    })
+    .then(function(){
+        return seedLocations();
     })
     .then(function () {
         console.log(chalk.green('Seed successful!'));
