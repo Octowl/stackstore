0faa185f052ea2ea1ada976afa507d20ed694944
Jenna Zenk <zenkjenna@gmail.com>
2016-06-08 13:05:57 -0400

    ok

--- browser/js/app.js
+++ browser/js/app.js
@@ -11,7 +11,7 @@ app.config(function ($urlRouterProvider, $locationProvider) {
         window.location.reload();
     });
 
-    $urlRouterProvider.when('/', '/grid');
+    $urlRouterProvider.when('/', '/products/grid');
 });
 
 // This app.run is for controlling access to specific states.
--- browser/js/products/product.states.js
+++ browser/js/products/product.states.js
@@ -1,12 +1,24 @@
 app.config(function ($stateProvider) {
-    $stateProvider.state('grid', {
-    	url : '/grid',
-    	templateUrl : 'js/products/productGrid.html',
-    	controller : 'ProductListCrtl',
+    $stateProvider.state('products', {
+        url : '/products',
+        templateUrl : 'js/products/products.html',
+        controller : 'ProductListCrtl',
         resolve: {
             products: function(Product) {
                 return Product.getAll();
             }
         }
     });
+
+    $stateProvider.state('products.grid', {
+        url : '/grid',
+        templateUrl : 'js/products/productGrid.html',
+        // controller : 'ProductListCrtl',
+        // resolve: {
+        //     products: function(Product) {
+        //         return Product.getAll();
+        //     }
+        // }
+    });
+
 });
