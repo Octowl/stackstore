468f133296689d46ec3bb120a37617269be33e61
Octowl <alsaff1987@gmail.com>
2016-06-08 19:02:59 -0400

    Add product location association

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,4 @@
-/*jshint node:true*/
+/* jshint node:true */
 'use strict';
 var db = require('./_db');
 module.exports = db;
@@ -8,3 +8,8 @@ require('./models/product')(db);
 require('./models/reviews')(db);
 require('./models/location')(db);
 require('./models/orders')(db);
+
+var Product = db.model('product');
+var Location = db.model('location');
+
+Product.belongsTo(Location);
